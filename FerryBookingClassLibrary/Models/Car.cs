8dc927ad630b7d1f9491b59cea1c6488b5561443
Lukas <lukasjp11@gmail.com>
2024-05-15 21:06:26 +0200

    2.0.1

--- FerryBookingClassLibrary/Models/Car.cs
+++ FerryBookingClassLibrary/Models/Car.cs
@@ -1,15 +1,29 @@
-﻿using FerryBookingClassLibrary.Models;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-public class Car
+namespace FerryBookingClassLibrary.Models
 {
-    public int Id { get; set; }
+    public class Car
+    {
+        public int Id { get; set; }
 
-    [Required]
-    [MinLength(1, ErrorMessage = "The car must have at least 1 guest.")]
-    [MaxLength(5, ErrorMessage = "The car can have a maximum of 5 guests.")]
-    public List<Guest> Guests { get; set; } = new List<Guest>();
-    public int FerryId { get; set; }
-    public Ferry Ferry { get; set; }
+        [Required]
+        [MinLength(1, ErrorMessage = "The car must have at least 1 guest.")]
+        [MaxLength(5, ErrorMessage = "The car can have a maximum of 5 guests.")]
+        public List<Guest> Guests { get; set; } = new List<Guest>();
+        public int FerryId { get; set; }
+        public Ferry Ferry { get; set; }
+
+        public void AddGuest(Guest guest)
+        {
+            if (Guests.Count >= 5)
+                throw new InvalidOperationException("Cannot add more guests to the car, maximum limit reached.");
+            Guests.Add(guest);
+        }
+
+        public void RemoveGuest(Guest guest)
+        {
+            Guests.Remove(guest);
+        }
+    }
 }
--- FerryBookingClassLibrary/Models/Ferry.cs
+++ FerryBookingClassLibrary/Models/Ferry.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FerryBookingClassLibrary.Models
 {
@@ -13,7 +15,44 @@ namespace FerryBookingClassLibrary.Models
         public List<Car> Cars { get; set; } = new List<Car>();
         public List<Guest> Guests { get; set; } = new List<Guest>();
 
-        public bool CanAddCar() => Cars.Count < MaxCars;
-        public bool CanAddGuest() => Guests.Count < MaxGuests;
+        public void AddGuest(Guest guest)
+        {
+            if (Guests.Count >= MaxGuests)
+                throw new InvalidOperationException("Cannot add more guests, ferry is full.");
+            Guests.Add(guest);
+        }
+
+        public void RemoveGuest(Guest guest)
+        {
+            Guests.Remove(guest);
+        }
+
+        public void AddCar(Car car)
+        {
+            if (Cars.Count >= MaxCars)
+                throw new InvalidOperationException("Cannot add more cars, ferry is full.");
+            Cars.Add(car);
+            foreach (var guest in car.Guests)
+            {
+                AddGuest(guest);
+            }
+        }
+
+        public void RemoveCar(Car car)
+        {
+            Cars.Remove(car);
+            foreach (var guest in car.Guests)
+            {
+                Guests.Remove(guest);
+            }
+        }
+
+        public decimal CalculateTotalPrice()
+        {
+            decimal total = 0;
+            total += Cars.Count * PricePerCar;
+            total += Guests.Count * PricePerGuest;
+            return total;
+        }
     }
 }
